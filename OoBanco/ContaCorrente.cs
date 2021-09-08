fbaaaca1dca9f4b99b6f460649043b0bdaff8a34
SoRePeSi <79937383+SoRePeSi@users.noreply.github.com>
2021-09-08 09:36:02 -0300

    Projeto criado

---
+++ OoBanco/ContaCorrente.cs
@@ -0,0 +1,6 @@
+using System;
+public class ContaCorrente{
+    public string titular;
+    public int agencia, conta;
+    public double saldo;
+}
---
+++ OoBanco/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OoBanco{
+    class Program{
+        static void Main(string[] args){
+            ContaCorrente ContaBarbara = new ContaCorrente();
+            ContaCorrente ContaJoao = new ContaCorrente();
+            
+            ContaBarbara.titular = "Bárbara";
+            ContaBarbara.agencia = 1234;
+            ContaBarbara.conta = 789;
+            ContaBarbara.saldo = 100.50;
+            
+            ContaJoao.titular = "João";
+            ContaJoao.agencia = 1234;
+            ContaJoao.conta = 790;
+            ContaJoao.saldo = 120;
+
+            Console.WriteLine("O titular da conta é {0}", ContaBarbara.titular);
+            Console.WriteLine("O titular da conta é {0}", ContaJoao.titular);
+        }
+    }
+}
