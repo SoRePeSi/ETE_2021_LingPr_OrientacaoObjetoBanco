0de7ddfb8f6ba26d8490e87f82e30fa08d0783e6
SoRePeSi <79937383+SoRePeSi@users.noreply.github.com>
2021-09-09 09:35:31 -0300

    Construtor vazio

--- OoBanco/ContaCorrente.cs
+++ OoBanco/ContaCorrente.cs
@@ -3,7 +3,8 @@ public class ContaCorrente{
     public string titular;
     public int agencia, conta;
     public double saldo;
-    public void CriarConta(string titular, int agencia, int conta, double saldo){
+    public ContaCorrente(){}
+    public ContaCorrente(string titular, int agencia, int conta, double saldo){
         this.titular = titular;
         this.agencia = agencia;
         this.conta = conta;
