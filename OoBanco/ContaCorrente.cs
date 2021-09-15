d9b32eafdb55301e30e030b66818ce37241d9222
SoRePeSi <79937383+SoRePeSi@users.noreply.github.com>
2021-09-15 09:38:29 -0300

    Adicionados métodos de Saque, Depósito e Transferência

--- OoBanco/ContaCorrente.cs
+++ OoBanco/ContaCorrente.cs
@@ -10,4 +10,47 @@ public class ContaCorrente{
         this.conta = conta;
         this.saldo = saldo;
     }
+
+    public bool Sacar(double valor){
+        if((this.saldo<valor)||(valor<0)){
+            return false;
+        }
+        else{
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public bool Depositar(double valor){
+        if(valor<=0){
+            Console.WriteLine("ERRO\nValor a depositar deve ser positivo");
+            return false;
+        }
+        else{
+            this.saldo += valor;
+
+            Console.WriteLine("Operação finalizada.");
+            return true;
+        }
+    }
+
+    public bool Transferir(ContaCorrente receptor, double valor){
+        if((valor<0)||(this.saldo<valor)){
+            return false;
+        }
+        else{
+            try{
+                this.Sacar(valor);
+                receptor.Depositar(valor);
+
+                Console.WriteLine("Operação finalizada");
+                return true;
+            }
+            catch{
+                Console.WriteLine("ERRO:\nConta inexistente.");
+                return false;
+            }
+        }
+    }
+
 }
--- OoBanco/Program.cs
+++ OoBanco/Program.cs
@@ -3,13 +3,12 @@
 namespace OoBanco{
     class Program{
         static void Main(string[] args){
-            ContaCorrente ContaBarbara = new ContaCorrente();
-            ContaBarbara.CriarConta("Bárbara", 1234, 789, 100.50);
-            ContaCorrente ContaJoao = new ContaCorrente();
-            ContaJoao.CriarConta("João", 1234, 790, 120);
+            ContaCorrente ContaBarbara = new ContaCorrente("Bárbara", 1234, 789, 100.50);
+            ContaCorrente ContaJoao = new ContaCorrente("João", 1245, 790, 120);
+            ContaCorrente ContaSofia = new ContaCorrente("Sofia", 1357, 799, 50);
 
-            Console.WriteLine("O titular da conta é {0}", ContaBarbara.titular);
-            Console.WriteLine("O titular da conta é {0}", ContaJoao.titular);
+            Console.WriteLine("O titular da conta é {0}. Valor: {1}", ContaBarbara.titular, ContaBarbara.saldo);
+            ContaBarbara.Sacar(100);
         }
     }
 }
